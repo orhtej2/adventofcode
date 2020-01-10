0e5985b02ebb128192856fa682955a966dc7a0d6
Mateusz Szymański <orhtej2@gmail.com>
2020-01-10 19:17:30 +0100

    Even more tests, now complete suite

--- 2019/python/aoccpu/testiopcodes.py
+++ 2019/python/aoccpu/testiopcodes.py
@@ -39,6 +39,17 @@ class TestOpcodes(TestCase):
         mock1.assert_called_once_with(program, 7)
         mock2.assert_called_once_with(2)
 
+    def test_o_output_rethrows(self):
+        program = [0, 0, 7]
+        mock1 = mock.MagicMock(return_value = 2)
+        mock2 = mock.MagicMock(side_effect=Exception())
+        with self.assertRaises(opcodes.FunctionThrownException) as ex:
+            opcodes.o_output(mock2, program, 1, mock1)
+        mock1.assert_called_once_with(program, 7)
+        mock2.assert_called_once_with(2)
+        self.assertIsInstance(ex.exception.outer, Exception)
+        self.assertEqual(3, ex.exception.step)
+
     def test_o_jumpIfTrue_true(self):
         program = [0, 2, 1, 7, 2]
         mock1 = mock.MagicMock(return_value = 1)
@@ -71,3 +82,46 @@ class TestOpcodes(TestCase):
         mock1.assert_called_once_with(program, 7)
         mock2.assert_not_called()
 
+    def test_o_lessThan_false(self):
+        program = [0, 2, 1, 7, 2, 12]
+        mock1 = mock.MagicMock(return_value = 5)
+        mock2 = mock.MagicMock(return_value = 2)
+        mock3 = mock.MagicMock()
+        self.assertEqual(6, opcodes.o_lessThan(program, 2, mock1, mock2, mock3))
+        mock1.assert_called_once_with(program, 7)
+        mock2.assert_called_once_with(program, 2)
+        mock3.assert_called_once_with(program, 12, True, 0)
+
+    def test_o_lessThan_true(self):
+        program = [0, 2, 1, 7, 2, 12]
+        mock1 = mock.MagicMock(return_value = 1)
+        mock2 = mock.MagicMock(return_value = 2)
+        mock3 = mock.MagicMock()
+        self.assertEqual(6, opcodes.o_lessThan(program, 2, mock1, mock2, mock3))
+        mock1.assert_called_once_with(program, 7)
+        mock2.assert_called_once_with(program, 2)
+        mock3.assert_called_once_with(program, 12, True, 1)
+
+    def test_o_equal_false(self):
+        program = [0, 2, 1, 7, 2, 12]
+        mock1 = mock.MagicMock(return_value = 5)
+        mock2 = mock.MagicMock(return_value = 2)
+        mock3 = mock.MagicMock()
+        self.assertEqual(6, opcodes.o_equal(program, 2, mock1, mock2, mock3))
+        mock1.assert_called_once_with(program, 7)
+        mock2.assert_called_once_with(program, 2)
+        mock3.assert_called_once_with(program, 12, True, 0)
+
+    def test_o_equal_true(self):
+        program = [0, 2, 1, 7, 2, 12]
+        mock1 = mock.MagicMock(return_value = 55)
+        mock2 = mock.MagicMock(return_value = 55)
+        mock3 = mock.MagicMock()
+        self.assertEqual(6, opcodes.o_equal(program, 2, mock1, mock2, mock3))
+        mock1.assert_called_once_with(program, 7)
+        mock2.assert_called_once_with(program, 2)
+        mock3.assert_called_once_with(program, 12, True, 1)
+    
+    def test_o_done(self):
+        program = [99]
+        self.assertRaises(opcodes.BreakoutException, opcodes.o_done, program, 0)
