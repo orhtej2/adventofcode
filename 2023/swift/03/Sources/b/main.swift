516f78659336709f3a4490534bff6a5f21989e63
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-12-03 18:03:10 +0100

    Solved 2023 day 3 part 2 in Swift.

--- 2023/swift/03/Sources/b/main.swift
+++ 2023/swift/03/Sources/b/main.swift
@@ -19,12 +19,56 @@ extension String {
     }
 }
 
-
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
 
 @main
 struct Day01a: ParsableCommand {
   @Option(help: "Specify the input")
   public var input: String
+
+  func touchesStar(schematic: [String], x: Int, y: Int) -> Point? {
+        for i in -1...1 {
+            for j in -1...1 {
+                if i == 0 && j == 0 {
+                    continue
+                }
+                if schematic[y + i][x + j] == "*" {
+                    return Point(x: x + j, y: y + i)
+                }
+            }
+        }
+        return nil
+    }
+
+   func getNumbersForLine(schematic: [String], y: Int) -> [Point: [Int]] {
+        var result = [Point: [Int]]()
+        var j = 1
+        var start = -1
+        while j < schematic[y].length-1 {
+            if schematic[y][j].first!.isNumber {
+                if start == -1 {
+                    start = j
+                }
+            } else {
+                start = -1
+            }
+            if start != -1, let point = touchesStar(schematic: schematic, x: j, y: y) {
+                while schematic[y][j].first!.isNumber {
+                    j += 1
+                }
+                let numberString = schematic[y][start..<j]
+                let value = Int(numberString)!
+                result[point] = result[point] ?? [Int]()
+                result[point]!.append(value)
+                start = -1
+            }
+            j += 1
+        }
+        return result
+    }
 
   public func run() throws {
     guard let filecontent = try? String(contentsOfFile: input, encoding: .utf8)
@@ -34,7 +78,7 @@ struct Day01a: ParsableCommand {
     }
     var schematic = [String]()
     for line in filecontent.components(separatedBy: .newlines) {
-        if (line.isEmpty) {
+        if line.isEmpty {
             break
         }
         schematic.append("." + line + ".")
@@ -42,44 +86,19 @@ struct Day01a: ParsableCommand {
     let filler: String = "".padding(toLength: schematic[0].lengthOfBytes(using: .utf8), withPad: ".", startingAt: 0)
     schematic.insert(filler, at: 0)
     schematic.append(filler)
-    let touchesPart = { (schematic: [String], x: Int, y: Int) -> Bool in
-        for i in -1...1 {
-            for j in -1...1 {
-                if i == 0 && j == 0 {
-                    continue
-                }
-                if schematic[y + i][x + j] != "." && !schematic[y + i][x + j].first!.isNumber {
-                    return true
-                }
-            }
-        }
-        return false;// schematic[y].row.index(row.startIndex, offsetBy: x) != "."
-    }
-    let width = schematic[0].lengthOfBytes(using: .utf8)
-    var result: Int = 0
+
+    var gears = [Point: [Int]]()
     for i in 1...schematic.count-1 {
-        var j = 1
-        var start = -1
-        while j < width {
-            if schematic[i][j].first!.isNumber {
-                if start == -1 {
-                    start = j
-                }
-            } else {
-                start = -1
-            }
-            if start != -1 && touchesPart(schematic, j, i) {
-                while schematic[i][j].first!.isNumber {
-                    j += 1
-                }
-                let numberString = schematic[i][start..<j]
-                // print(numberString)
-                let value = Int(numberString)!
-                print(value)
-                result += value
-                start = -1
-            }
-            j += 1
+        gears.merge(getNumbersForLine(schematic: schematic, y: i)) { (current, new) in
+            return current + new
+        }
+    }
+
+    var result: Int = 0
+    for (key, value) in gears {
+        if value.count == 2 {
+            print("Found gear at \(key) with \(value)")
+            result += value[0] * value[1]
         }
     }
     print("Result: \(result)")
